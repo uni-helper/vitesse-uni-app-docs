c2ac94992b91e0ba1923d4f51f64492ce0422d5c
KeJunMao <kejun1997@gmail.com>
2023-11-08 08:52:08 +0800

    docs: update theme and site meta

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,8 +2,78 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  lang: 'zh-CN',
   title: 'Vitesse uni-app',
-  description: 'Vite & UniApp Powered Starter Template',
+  description: 'Vite & uni-app Powered Starter Template',
+  lastUpdated: true,
+  cleanUrls: true,
+  head: [
+    [
+      'meta',
+      {
+        name: 'twitter:title',
+        content:
+          'Vite & uni-app Powered Starter Template',
+      },
+    ],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:site', content: '@uni-helper' }],
+    [
+      'meta',
+      {
+        name: 'twitter:image:src',
+        content:
+          'https://opengraph.githubassets.com/1cac1150838995e1f7d1643c00eee51a5d884f2054f995c9d3225b07b0eddb39/uni-helper/vitesse-uni-app',
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:image',
+        content:
+          'https://opengraph.githubassets.com/1cac1150838995e1f7d1643c00eee51a5d884f2054f995c9d3225b07b0eddb39/uni-helper/vitesse-uni-app',
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:image:alt',
+        content:
+          'Vite & uni-app Powered Starter Template',
+      },
+    ],
+    ['meta', { property: 'og:image:width', content: '1200' }],
+    ['meta', { property: 'og:image:height', content: '600' }],
+    ['meta', { property: 'og:site_name', content: 'GitHub' }],
+    ['meta', { property: 'og:type', content: 'object' }],
+    [
+      'meta',
+      {
+        property: 'og:title',
+        content:
+          'Vite & uni-app Powered Starter Template',
+      },
+    ],
+    [
+      'meta',
+      { property: 'og:url', content: 'https://github.com/uni-helper/vitesse-uni-app' },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:description',
+        content:
+          'Vite & uni-app Powered Starter Template',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/logo.svg',
+      },
+    ],
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: '/logo.svg',
